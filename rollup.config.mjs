fe44342b1a20c7bd6da2ef9dcc48b7ec4f1e3089
Lorenzo Elenkov <lorenzoelenkov@coherentsolutions.com>
2023-01-29 21:21:40 +0200

    Made changes to peerDependecies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,8 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
 import packageJson from './package.json' assert { type: 'json' };
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
@@ -20,12 +22,18 @@ export default [
         sourcemap: true
       }
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), postcss()]
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss()
+    ]
   },
   {
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
+    plugins: [dts(), terser()],
     external: [/\.css$/]
   }
 ];
